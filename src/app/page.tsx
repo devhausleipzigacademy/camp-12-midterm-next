047248fbfa5a1084f17882b4b75f0e27d5455186
Dan McAtee <danielmcatee@me.com>
2024-07-26 16:46:32 +0200

    fix typescript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 export default async function Page() {
-  const todos = await fetch("https://jsonplaceholder.typicode.com/todos").then(
-    (res) => res.json()
-  );
+  const todos: any = await fetch(
+    "https://jsonplaceholder.typicode.com/todos"
+  ).then((res) => res.json());
 
   return (
     <div>
       <span>Homepage</span>
       <ul className="grid grid-cols-3 gap-4">
-        {todos.map((todo) => (
+        {todos.map((todo: any) => (
           <li key={todo.id}>{todo.title}</li>
         ))}
       </ul>
