5f385f4638e062c45487ae1c180cfef0f592f2cd
HaillSeitan <150841231+HaillSeitan@users.noreply.github.com>
2024-07-29 12:18:00 +0200

    port cast page

--- src/app/(full-page)/movies/[movieId]/cast/page.tsx
+++ src/app/(full-page)/movies/[movieId]/cast/page.tsx
@@ -1,11 +1,39 @@
-export default function CastAndCrewPage({
-  params,
-}: {
-  params: { movieId: string };
-}) {
+import { MovieCredit, MovieDetail } from "@/lib/types/movie";
+import axios from "axios";
+import { Switcher } from "./switcher";
+
+type GroupedCrewMember = {
+  name: string;
+  profile_path: string | null;
+  jobs: string[];
+};
+
+const CastPage = async ({ params }: { params: { movieId: string } }) => {
+  // toggle between cast and crew
+  const movie = await axios
+    .get<MovieDetail>(`https://api.themoviedb.org/3/movie/${params.movieId}`, {
+      params: {
+        api_key: process.env.TMDB_API_KEY,
+        language: "en-US",
+      },
+    })
+    .then((res) => res.data);
+  const credits = await axios
+    .get<MovieCredit>(
+      `https://api.themoviedb.org/3/movie/${params.movieId}/credits`,
+      {
+        params: {
+          api_key: process.env.TMDB_API_KEY,
+          language: "en-US",
+        },
+      }
+    )
+    .then((res) => res.data);
   return (
-    <div>
-      <span>Cast & Crew - {params.movieId}</span>
+    <div className="bg-dark h-fit">
+      <Switcher movie={movie} credits={credits} />
     </div>
   );
-}
+};
+
+export default CastPage;
