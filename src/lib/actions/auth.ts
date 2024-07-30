6681dba4ef61068a6f04d8b953e285010332bf87
Dan McAtee <danielmcatee@me.com>
2024-07-30 15:40:26 +0200

    add signout

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -4,7 +4,7 @@ import { generateIdFromEntropySize } from "lucia";
 import { z } from "zod";
 import { prisma } from "../db";
 import bcrypt from "bcrypt";
-import { lucia } from "../auth";
+import { lucia, validateRequest } from "../auth";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -115,4 +115,23 @@ export async function login(formData: FormData) {
     sessionCookie.attributes
   );
   return redirect("/");
+}
+
+export async function signOut() {
+  const { session } = await validateRequest();
+  if (!session) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  await lucia.invalidateSession(session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+  return redirect("/login");
 }
