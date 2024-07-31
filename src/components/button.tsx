4e8d8fdf7eac3761fad14c4c46a3d068c83788f8
Malibu <joellement@gmail.com>
2024-07-31 13:21:18 +0200

    On branch 7-port-select-seats  Changes to be committed: 	new file:   .vscode/settings.json 	new file:   package-lock.json 	modified:   prisma/schema.prisma 	modified:   src/app/(full-page)/movies/[movieId]/seats/page.tsx 	new file:   src/components/button.tsx 	new file:   src/components/seat-cart.tsx 	new file:   src/components/seat-map.tsx 	new file:   src/components/seat.tsx 	new file:   src/components/select-seats.tsx 	new file:   src/lib/seats.ts 	new file:   src/lib/styling.tsx 	new file:   src/lib/types/seats.ts 	modified:   tailwind.config.ts

---
+++ src/components/button.tsx
@@ -0,0 +1,36 @@
+import { cn } from "../lib/styling";
+
+type Props = {
+  children: React.ReactNode;
+  variant?: "primary" | "secondary";
+  type?: "button" | "submit";
+  size?: "default" | "small";
+  className?: string;
+  onClick?: () => void;
+};
+
+export function Button({
+  children,
+  variant = "primary",
+  size = "default",
+  type = "button",
+  className,
+  onClick,
+}: Props) {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      className={cn(
+        "rounded-lg font-semibold w-full",
+        variant === "primary" ? "bg-yellow disabled:bg-yellow/50" : null,
+        variant === "secondary" ? "bg-dark-light" : null,
+        size === "default" ? "py-4" : null,
+        size === "small" ? "text-xs py-3" : null,
+        className
+      )}
+    >
+      {children}
+    </button>
+  );
+}
